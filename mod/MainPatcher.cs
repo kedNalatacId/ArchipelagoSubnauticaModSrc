fe4e57e23a2f30ce13a3e704032edfd0edbb7a53
Fabian Dill <fabian.dill@web.de>
2022-12-31 15:36:24 +0100

    remove qmods interface qmods is discontinued, so no reason to keep it around

--- mod/MainPatcher.cs
+++ mod/MainPatcher.cs
@@ -6,39 +6,19 @@ using UnityEngine;
 
 namespace Archipelago
 {
-    //QMODS Interface
-    public class QModPatcher
-    {
-        public static void Patch()
-        {
-            APWrapper.Init();
-            Debug.Log($"Plugin Archipelago (" + APWrapper.Version + ") for Server (" 
-                      + APState.AP_VERSION[0] + "." + APState.AP_VERSION[1] + "." + APState.AP_VERSION[2] + 
-                      ") is loaded!");
-        }
-    }
     //BepInEx Interface
-    [BepInPlugin("Archipelago", "Archipelago", APWrapper.Version)]
+    [BepInPlugin("Archipelago", "Archipelago", Version)]
     public class ArchipelagoPlugin : BaseUnityPlugin
     {
+        public const string Version = "1.3.4";
         private void Awake()
         {
-            APWrapper.Init();
-            Logger.LogInfo($"Plugin Archipelago (" + APWrapper.Version + ") for Server (" 
-                           + APState.AP_VERSION[0] + "." + APState.AP_VERSION[1] + "." + APState.AP_VERSION[2] + 
-                           ") is loaded!");
-        }
-    }
-    //Common interface
-    public static class APWrapper
-    {
-        public const string Version = "1.3.4";
-        public static void Init()
-        {
-            // Plugin startup logic
             var harmony = new Harmony("Archipelago");
             APState.Init();
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Logger.LogInfo($"Plugin Archipelago (" + Version + ") for Server (" 
+                           + APState.AP_VERSION[0] + "." + APState.AP_VERSION[1] + "." + APState.AP_VERSION[2] + 
+                           ") is loaded!");
         }
     }
 }
